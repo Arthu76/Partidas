2864038e52ad5ec9cb8e4c2b3f632195e9c19623
Arthur <arthurhensilva18@gmail.com>
2023-05-04 21:36:05 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Option from './components/Option';
+
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [anoSelecionado, setAnoSelecionado] = useState('');
+  const [dadosCampeonato, setDadosCampeonato] = useState([]);
+
+  function alterarAno(event) {
+    const ano = event.target.value;
+    setAnoSelecionado(ano);
+
+    axios.get(`./allYears/${ano}/${ano}.json`).then(response => {
+      let data = response.data;
+      console.log(data);
+      let numero = data.map(element => {
+        return element.numero;
+      });
+      let partidas = data.map(element => {
+        return element.partidas;
+      });
+      // console.log(partidas);
+      let test = partidas.map(element => {
+        // console.log(element);
+        return element.visitante;
+      });
+      console.log(test);
+      let visitantes = test.map(element => {
+        return element.visitante;
+      });
+      // console.log(visitantes);
+      setDadosCampeonato(`${numero}: ${visitantes}`);
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>React campeonato Brasileiro</Header>
+      <select name="" id="" onChange={alterarAno}>
+        <Option>2003</Option>
+        <Option>2004</Option>
+        <Option>2005</Option>
+        <Option>2006</Option>
+      </select>
+      <p>{dadosCampeonato}</p>
     </div>
   );
 }
